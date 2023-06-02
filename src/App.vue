a134648f9a412cf6e60a873a7e63eb9c424d1322
BryanMureithi <brymureithi@gmail.com>
2023-06-02 21:31:44 +0300

    restart

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <template>
   <NavBar />
-  <main>
-    <HeroMain />
-    <AboutMain />
-  </main>
 </template>
 
 <script>
 import NavBar from './components/Navbar.vue';
-import HeroMain from './components/Hero.vue';
-import AboutMain from './components/About.vue';
 
 export default {
   name: 'App',
   components: {
     NavBar,
-    HeroMain,
-    AboutMain
     },
 }
 </script>
@@ -25,6 +17,7 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Poppins:wght@400;700&display=swap');
 body{
   font-family: 'Inter', sans-serif;
+  background-color: #E0E1DD;
 }
 
 </style>
